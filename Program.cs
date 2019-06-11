1f5f3de242db90e0c9130be9acf647554a7e4484
Allan Hall <bigal76126@gmail.com>
2019-06-11 16:23:08 -0400

    doing stuff

--- Program.cs
+++ Program.cs
@@ -9,11 +9,27 @@ namespace NumberGuesser
       Console.WriteLine("Hello World!");
       const int min = 1;
       const int max = 100;
-      int lower = min;
-      int upper = max;
+      var lower = min;
+      var upper = max;
+      int tries = 0;
       double nextGuess = Math.Round(Math.Log(max) / Math.Log(2));
+      var guess = (lower + upper) / 2;
+      var correctGuess = 33;
 
+      Console.WriteLine(nextGuess);
+      while (correctGuess != nextGuess)
+      {
+        if (nextGuess > correctGuess)
+        {
+          return (lower = guess + 1) && tries++;
+        }
+        if (nextGuess < correctGuess)
+        {
+          return (upper = guess - 1) && tries++;
 
+        }
+
+      }
     }
   }
 }
