698e50c281cdac9cb4888dfca8bfc5aa8b61e7b8
Allan Hall <bigal76126@gmail.com>
2019-06-11 15:23:32 -0400

    did stuff

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      const int min = 1;
+      const int max = 100;
+      int lower = min;
+      int upper = max;
+      double nextGuess = Math.Round(Math.Log(max) / Math.Log(2));
+
+
+    }
+  }
+}
