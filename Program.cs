a17bb0a95030a0ef9be6f1e430d9af762185eb40
Allan Hall <bigal76126@gmail.com>
2019-06-11 16:54:54 -0400

    kinda got it working

--- Program.cs
+++ Program.cs
@@ -13,23 +13,28 @@ namespace NumberGuesser
       var upper = max;
       var tries = 0;
       double nextGuess = Math.Round(Math.Log(max) / Math.Log(2));
-      var guess = (lower + upper) / 2;
-
-      Console.WriteLine($"Is {guess} your number?");
-      var input = Console.ReadLine();
+      var input = "";
       while (input != "Yes")
       {
-        Console.WriteLine("Is it higher or lower?");
-        if (input == "higher")
+        var guess = (lower + upper) / 2;
+        Console.WriteLine($"Is {guess} your number?");
+        input = Console.ReadLine();
+        if (input == "no")
         {
-          lower = guess + 1;
-          tries++;
-        }
-        if (input == "lower")
-        {
-          upper = guess - 1;
-          tries++;
+          Console.WriteLine("Is it higher or lower?");
+          input = Console.ReadLine();
 
+          if (input == "higher")
+          {
+            lower = guess + 1;
+            tries++;
+            Console.Write($"Is {guess} your number?");
+          }
+          if (input == "lower")
+          {
+            upper = guess - 1;
+            tries++;
+          }
 
         }
 
