c2180b610a4c54a46de39fea936c7f7e98fcc269
Allan Hall <bigal76126@gmail.com>
2019-06-11 22:33:42 -0400

    added a play again option

--- Program.cs
+++ Program.cs
@@ -6,38 +6,50 @@ namespace NumberGuesser
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
-      const int min = 1;
-      const int max = 100;
-      var lower = min;
-      var upper = max;
-      var tries = 0;
-      double nextGuess = Math.Round(Math.Log(max) / Math.Log(2));
-      var input = "";
-      while (input != "Yes")
+      bool again = false;
+      while (!again)
       {
-        var guess = (lower + upper) / 2;
-        Console.WriteLine($"Is {guess} your number?");
-        input = Console.ReadLine();
-        if (input == "no")
-        {
-          Console.WriteLine("Is it higher or lower?");
-          input = Console.ReadLine();
+        Console.WriteLine("Welcome! Pick a number between 1 and 100 and I will figure it out...");
+        const int min = 1;
+        const int max = 100;
+        var lower = min;
+        var upper = max;
+        var tries = 0;
+        double nextGuess = Math.Round(Math.Log(max) / Math.Log(2));
+        var input = "";
 
-          if (input == "higher")
+        while (input != "yes")
+        {
+          var guess = (lower + upper) / 2;
+          Console.WriteLine($"Is {guess} your number?");
+          input = Console.ReadLine();
+          if (input == "no")
           {
-            lower = guess + 1;
-            tries++;
-            Console.Write($"Is {guess} your number?");
-          }
-          if (input == "lower")
-          {
-            upper = guess - 1;
-            tries++;
+            Console.WriteLine("Is it higher or lower?");
+            input = Console.ReadLine();
+
+            if (input == "higher")
+            {
+              lower = guess + 1;
+              tries++;
+            }
+            if (input == "lower")
+            {
+              upper = guess - 1;
+              tries++;
+            }
+
           }
 
         }
-
+        if (input == "yes")
+        {
+          Console.WriteLine("I knew I'd get it! Press 'n' if you would like to play again");
+        }
+        if (Console.ReadLine() == "n")
+        {
+          again = false;
+        }
       }
     }
   }
